4f6a538689c6ac9797288db88c445962ce8be289
Pawan Mehta <pawan@openetech.com>
2020-06-25 17:10:03 +0530

    fix: v2

--- aprequest/ap_request/doctype/ap_request/ap_request.py
+++ aprequest/ap_request/doctype/ap_request/ap_request.py
@@ -7,4 +7,30 @@ import frappe
 from frappe.model.document import Document
 
 class APRequest(Document):
-	pass
+	def validate(self):
+		def default_questions():
+			table = "question"
+			rev_question_detail = list(frappe.db.sql("""
+									select 
+										question
+									from 
+										`tabQuestion Detail`
+									where parent = %s""", "APR", as_dict=1))
+			self.set(table, [])
+			for d in rev_question_detail:
+				self.append(table, d)
+			for d in self.question:
+				d.response = "No"
+		if self.get("__islocal") == 1 and not self.question:
+			default_questions()
+		else:
+			if self.question:
+				pass
+			else:
+				default_questions()
+	def on_update(self):
+		pass
+	def on_submit(self):
+		pass
+	def on_cancel(self):
+		pass
---
+++ aprequest/custom_method.py
@@ -0,0 +1,36 @@
+from __future__ import unicode_literals
+import frappe
+import frappe.defaults
+from frappe.utils import cint, flt, add_months, today, date_diff, getdate, add_days, cstr, time_diff_in_hours
+from frappe import _
+from frappe.model.mapper import get_mapped_doc
+from datetime import datetime
+import re
+
+def split_apr(docname):
+	#loop through count and make that many APRs, copy similar fields
+	apr_doc = frappe.get_doc('AP Request', docname)
+	split_no = apr_doc.no_of_split
+	if split_no > 0:
+		child_req = ''
+		for x in range(split_no):
+			ap_doc = frappe.new_doc('AP Request')
+			ap_doc.apr_date =  today()
+			ap_doc.apr_status = 'Initiated'
+			ap_doc.supplier = apr_doc.supplier
+			ap_doc.parent_apr = apr_doc.name
+			ap_doc.insert(ignore_mandatory=True, ignore_permissions=True)
+			child_req = ap_doc.name + '/'
+
+		apr_doc.child_apr = child_req
+		apr_doc.update(ignore_permissions=True)
+
+def make_apr(docname):
+	#make apr from issue
+	issue_doc = frappe.get_doc('Issue', docname)
+	apr_doc = frappe.new_doc('AP Request')
+	apr_doc.apr_date =  today()
+	apr_doc.apr_status = 'Initiated'
+	apr_doc.apr_assigned_to = issue_doc.apr_assigned_to
+	apr_doc.supplier = issue_doc.supplier
+	apr_doc.insert(ignore_mandatory=True, ignore_permissions=True)
