2747532315d545921613bcded084d78fba963145
Pawan Mehta <pawan@openetech.com>
2020-07-08 08:52:47 +0530

    fix: count

--- aprequest/custom_method.py
+++ aprequest/custom_method.py
@@ -187,7 +187,7 @@ def update_count(self, method):
 						attached_to_doctype = 'Issue' and
 						attached_to_name = %s
 					''', (self.attached_to_name), as_list=1)
-		if flt(count[0][0]) > 0:
+		if flt(count[0][0]) >= 0:
 			issue_doc = frappe.get_doc('Issue', self.attached_to_name)
 			issue_doc.attachment_check_total = count[0][0]
 			issue_doc.save()
